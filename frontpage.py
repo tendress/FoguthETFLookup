d4c0de17108b63105f181e858ee497eb4b8d37c7
tendress <anthony.endress@gmail.com>
2025-05-05 11:13:33 -0400

    font

--- frontpage.py
+++ frontpage.py
@@ -46,7 +46,8 @@ if selected_page == "Home":
         )
     st.markdown(
         """
-        <h1 style="color: red; font-size: 32px;">Sector Rotation Rebalance: Qualified 5/2 (Complete), Non-Qualified 5/5 (Complete)</h1>
+        <h1 style="color: red; font-size: 32px;">Sector Rotation Rebalance: </h2>
+        <h2 style="color: red; font-size: 24px;">Qualified 5/2 (Complete), Non-Qualified 5/5 (Complete)</h2>
         <ul style="color: red; font-size: 20px;">
             <li>XLE: The Energy Select Sector SPDR Fund - 25%</li>
             <li>XLU: The Utilities Select Sector SPDR Fund - 20%</li>
