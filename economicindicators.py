a432d5c30694bbc43a3e67d439844b2bb0fb1ee1
tendress <anthony.endress@gmail.com>
2025-05-13 16:04:00 -0400

    a

--- economicindicators.py
+++ economicindicators.py
@@ -259,6 +259,14 @@ def economic_indicators():
                     title='Bond Yields: 3-Month, 5-Year, 10-Year, 30-Year')
         st.plotly_chart(fig, use_container_width=True)
 
+        # Display the most recent value for 10-Year Minus 2-Year Treasury Yield
+        if not df2.empty:
+            most_recent_t10y2y = df2.iloc[-1]['Close']
+            st.write(f"10-Year Minus 2-Year Treasury Yield: {most_recent_t10y2y:.2f}%")
+            st.markdown(
+            "<span style='color:#0066CC; font-weight:bold;'>The difference between the 10-year and the 2-year Treasury yields, known as the Yield Curve Spread, indicates investor expectations about future economic growth and monetary policy. A positive spread suggests cautious optimism for economic expansion while a negative spread (inverted yield curve) often signals recession risk.</span>",
+            unsafe_allow_html=True)
+
         # Display a graph of the 10-Year Minus 2-Year Treasury Yield
         fig2 = px.line(df2, x='Date', y='Close',
                     title='10-Year Minus 2-Year Treasury Yield')
@@ -271,13 +279,7 @@ def economic_indicators():
         fig2.update_layout(xaxis_title="Date", yaxis_title="Yield (%)")
         st.plotly_chart(fig2, use_container_width=True)
 
-        # Display the most recent value for 10-Year Minus 2-Year Treasury Yield
-        if not df2.empty:
-            most_recent_t10y2y = df2.iloc[-1]['Close']
-            st.write(f"10-Year Minus 2-Year Treasury Yield: {most_recent_t10y2y:.2f}%")
-            st.markdown(
-            "<span style='color:#0066CC; font-weight:bold;'>The difference between the 10-year and the 2-year Treasury yields, known as the Yield Curve Spread, indicates investor expectations about future economic growth and monetary policy. A positive spread suggests cautious optimism for economic expansion while a negative spread (inverted yield curve) often signals recession risk.</span>",
-            unsafe_allow_html=True)
+        
 
         # User selects a specific date
         st.subheader("Select a Date to View Bond Yields")
