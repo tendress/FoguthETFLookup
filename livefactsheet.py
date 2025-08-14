e88d75a646af76de91683faf2fde6e4020b981ca
tendress <anthony.endress@gmail.com>
2025-08-14 15:48:02 -0400

    updatelivefactsheet

--- livefactsheet.py
+++ livefactsheet.py
@@ -21,7 +21,7 @@ def display_live_factsheet():
         </style>
     """, unsafe_allow_html=True)
     
-    @st.cache_data
+    @st.cache_data(ttl=86400) 
     def load_etf_weights_for_model(selected_model):
         query = '''
             SELECT 
